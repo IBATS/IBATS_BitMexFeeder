a6151101ab3fda0f244c7a807f32d9b34e838019
mmmaaaggg <mmmaaaggg@163.com>
2018-10-11 18:08:47 +0800

    update init funcion

--- bitmexfeeder/feeder.py
+++ bitmexfeeder/feeder.py
@@ -11,13 +11,17 @@ from threading import Thread
 from bitmex_websocket import BitMEXWebsocket
 import bitmex
 from bitmexfeeder.config import config
-import logging
 from bitmexfeeder.utils.mess import load_against_pagination
 from sqlalchemy.types import String, Date, DateTime, Time, Integer, Boolean
 from sqlalchemy.dialects.mysql import DOUBLE
 from ibats_common.utils.db import bunch_insert_on_duplicate_update, with_db_session
 from bitmexfeeder.backend import engine_md
+import time
+from ibats_common.utils.mess import try_n_times
+import logging
+from datetime import datetime
 
+logger = logging.getLogger()
 DTYPE_INSTRUMENT = {
     'symbol': String(20),
     'rootSymbol': String(10),
@@ -264,131 +268,131 @@ class MDFeeder(Thread):
         while self.is_working:
             time.sleep(5)
 
-    def fill_history(self, periods=['1day', '1min', '60min']):
-        for period in periods:
-            if period == '1min':
-                model_tot, model_tmp = MDMin1, MDMin1Temp
-            elif period == '60min':
-                model_tot, model_tmp = MDMin60, MDMin60Temp
-            elif period == '1day':
-                model_tot, model_tmp = MDMinDaily, MDMinDailyTemp
-            else:
-                self.logger.warning(f'{period} 不是有效的周期')
+    # def fill_history(self, periods=['1day', '1min', '60min']):
+    #     for period in periods:
+    #         if period == '1min':
+    #             model_tot, model_tmp = MDMin1, MDMin1Temp
+    #         elif period == '60min':
+    #             model_tot, model_tmp = MDMin60, MDMin60Temp
+    #         elif period == '1day':
+    #             model_tot, model_tmp = MDMinDaily, MDMinDailyTemp
+    #         else:
+    #             self.logger.warning(f'{period} 不是有效的周期')
+    #
+    #         self.fill_history_period(period, model_tot, model_tmp)
 
-            self.fill_history_period(period, model_tot, model_tmp)
-
-    def fill_history_period(self, period, model_tot, model_tmp: MDMin1Temp):
-        """
-        根据数据库中的支持 symbol 补充历史数据
-        :param period:
-        :param model_tot:
-        :param model_tmp:
-        :return:
-        """
-        with with_db_session(engine_md) as session:
-            data = session.query(SymbolPair).filter(
-                SymbolPair.market == Config.MARKET_NAME).all()  # , SymbolPair.symbol_partition == 'main'
-            pair_datetime_latest_dic = dict(
-                session.query(
-                    model_tmp.symbol, func.max(model_tmp.ts_start)
-                ).filter(model_tmp.market == Config.MARKET_NAME).group_by(model_tmp.symbol).all()
-            )
-
-        # 循环获取每一个交易对的历史数据
-        for symbol_info in data:
-            symbol = f'{symbol_info.base_currency}{symbol_info.quote_currency}'
-            if symbol in pair_datetime_latest_dic:
-                datetime_latest = pair_datetime_latest_dic[symbol]
-                if period == '1min':
-                    second_of_period = 60
-                elif period == '60min':
-                    second_of_period = 60 * 60
-                elif period == '1day':
-                    second_of_period = 60 * 60 * 24
-                else:
-                    self.logger.warning(f'{period} 不是有效的周期')
-                    continue
-                size = min([2000, int((datetime.now() - datetime_latest).seconds / second_of_period * 1.2)])
-            else:
-                size = 2000
-            if size <= 1:
-                continue
-            ret = self.get_kline(symbol, period, size=size)
-            # for n in range(1, 3):
-            #     try:
-            #         ret = self.api.get_kline(symbol, period, size=size)
-            #     except ProxyError:
-            #         self.logger.exception('symbol:%s, period:%s, size=%d', symbol, period, size)
-            #         ret = None
-            #         time.sleep(5)
-            #         continue
-            #     break
-            if ret is None:
-                continue
-            if ret['status'] == 'ok':
-                data_list = ret['data']
-                data_dic_list = []
-                for data in data_list:
-                    ts_start = datetime.fromtimestamp(data.pop('id'))
-                    data['ts_start'] = ts_start
-                    data['market'] = Config.MARKET_NAME
-                    data['ts_curr'] = ts_start + timedelta(seconds=59)  # , microseconds=999999
-                    data['symbol'] = symbol
-                    data_dic_list.append(data)
-                self._save_md(data_dic_list, symbol, model_tot, model_tmp)
-            else:
-                self.logger.error(ret)
-            # 过于频繁方位可能导致链接失败
-            time.sleep(5)  # 已经包含在 try_n_times 里面
+    # def fill_history_period(self, period, model_tot, model_tmp: MDMin1Temp):
+    #     """
+    #     根据数据库中的支持 symbol 补充历史数据
+    #     :param period:
+    #     :param model_tot:
+    #     :param model_tmp:
+    #     :return:
+    #     """
+    #     with with_db_session(engine_md) as session:
+    #         data = session.query(SymbolPair).filter(
+    #             SymbolPair.market == Config.MARKET_NAME).all()  # , SymbolPair.symbol_partition == 'main'
+    #         pair_datetime_latest_dic = dict(
+    #             session.query(
+    #                 model_tmp.symbol, func.max(model_tmp.ts_start)
+    #             ).filter(model_tmp.market == Config.MARKET_NAME).group_by(model_tmp.symbol).all()
+    #         )
+    #
+    #     # 循环获取每一个交易对的历史数据
+    #     for symbol_info in data:
+    #         symbol = f'{symbol_info.base_currency}{symbol_info.quote_currency}'
+    #         if symbol in pair_datetime_latest_dic:
+    #             datetime_latest = pair_datetime_latest_dic[symbol]
+    #             if period == '1min':
+    #                 second_of_period = 60
+    #             elif period == '60min':
+    #                 second_of_period = 60 * 60
+    #             elif period == '1day':
+    #                 second_of_period = 60 * 60 * 24
+    #             else:
+    #                 self.logger.warning(f'{period} 不是有效的周期')
+    #                 continue
+    #             size = min([2000, int((datetime.now() - datetime_latest).seconds / second_of_period * 1.2)])
+    #         else:
+    #             size = 2000
+    #         if size <= 1:
+    #             continue
+    #         ret = self.get_kline(symbol, period, size=size)
+    #         # for n in range(1, 3):
+    #         #     try:
+    #         #         ret = self.api.get_kline(symbol, period, size=size)
+    #         #     except ProxyError:
+    #         #         self.logger.exception('symbol:%s, period:%s, size=%d', symbol, period, size)
+    #         #         ret = None
+    #         #         time.sleep(5)
+    #         #         continue
+    #         #     break
+    #         if ret is None:
+    #             continue
+    #         if ret['status'] == 'ok':
+    #             data_list = ret['data']
+    #             data_dic_list = []
+    #             for data in data_list:
+    #                 ts_start = datetime.fromtimestamp(data.pop('id'))
+    #                 data['ts_start'] = ts_start
+    #                 data['market'] = Config.MARKET_NAME
+    #                 data['ts_curr'] = ts_start + timedelta(seconds=59)  # , microseconds=999999
+    #                 data['symbol'] = symbol
+    #                 data_dic_list.append(data)
+    #             self._save_md(data_dic_list, symbol, model_tot, model_tmp)
+    #         else:
+    #             self.logger.error(ret)
+    #         # 过于频繁方位可能导致链接失败
+    #         time.sleep(5)  # 已经包含在 try_n_times 里面
 
     @try_n_times(5, sleep_time=5, logger=logger)
     def get_kline(self, symbol, period, size):
         ret = self.api.get_kline(symbol, period, size=size)
         return ret
 
-    def _save_md(self, data_dic_list, symbol, model_tot: MDMin1, model_tmp: MDMin1Temp):
-        """
-        保存md数据到数据库及文件
-        :param data_dic_list:
-        :param symbol:
-        :param model_tot:
-        :param model_tmp:
-        :return:
-        """
-
-        if data_dic_list is None or len(data_dic_list) == 0:
-            self.logger.warning("data_dic_list 为空")
-            return
-
-        md_count = len(data_dic_list)
-        # 保存到数据库
-        with with_db_session(engine_md) as session:
-            try:
-                # session.execute(self.md_orm_table_insert, data_dic_list)
-                session.execute(model_tmp.__table__.insert(on_duplicate_key_update=True), data_dic_list)
-                self.logger.info('%d 条 %s 历史数据 -> %s 完成', md_count, symbol, model_tmp.__tablename__)
-                sql_str = f"""insert into {model_tot.__tablename__} select * from {model_tmp.__tablename__} 
-                where market=:market and symbol=:symbol 
-                ON DUPLICATE KEY UPDATE open=VALUES(open), high=VALUES(high), low=VALUES(low), close=VALUES(close)
-                , amount=VALUES(amount), vol=VALUES(vol), count=VALUES(count)"""
-                session.execute(sql_str, params={"symbol": symbol, "market": Config.MARKET_NAME})
-                datetime_latest = session.query(
-                    func.max(model_tmp.ts_start).label('ts_start_latest')
-                ).filter(
-                    model_tmp.symbol == symbol,
-                    model_tmp.market == Config.MARKET_NAME
-                ).scalar()
-                # issue:
-                # https://stackoverflow.com/questions/9882358/how-to-delete-rows-from-a-table-using-an-sqlalchemy-query-without-orm
-                delete_count = session.query(model_tmp).filter(
-                    model_tmp.market == Config.MARKET_NAME,
-                    model_tmp.symbol == symbol,
-                    model_tmp.ts_start < datetime_latest
-                ).delete()
-                self.logger.debug('%d 条 %s 历史数据被清理，最新数据日期 %s', delete_count, symbol, datetime_latest)
-                session.commit()
-            except:
-                self.logger.exception('%d 条 %s 数据-> %s 失败', md_count, symbol, model_tot.__tablename__)
+    # def _save_md(self, data_dic_list, symbol, model_tot: MDMin1, model_tmp: MDMin1Temp):
+    #     """
+    #     保存md数据到数据库及文件
+    #     :param data_dic_list:
+    #     :param symbol:
+    #     :param model_tot:
+    #     :param model_tmp:
+    #     :return:
+    #     """
+    #
+    #     if data_dic_list is None or len(data_dic_list) == 0:
+    #         self.logger.warning("data_dic_list 为空")
+    #         return
+    #
+    #     md_count = len(data_dic_list)
+    #     # 保存到数据库
+    #     with with_db_session(engine_md) as session:
+    #         try:
+    #             # session.execute(self.md_orm_table_insert, data_dic_list)
+    #             session.execute(model_tmp.__table__.insert(on_duplicate_key_update=True), data_dic_list)
+    #             self.logger.info('%d 条 %s 历史数据 -> %s 完成', md_count, symbol, model_tmp.__tablename__)
+    #             sql_str = f"""insert into {model_tot.__tablename__} select * from {model_tmp.__tablename__}
+    #             where market=:market and symbol=:symbol
+    #             ON DUPLICATE KEY UPDATE open=VALUES(open), high=VALUES(high), low=VALUES(low), close=VALUES(close)
+    #             , amount=VALUES(amount), vol=VALUES(vol), count=VALUES(count)"""
+    #             session.execute(sql_str, params={"symbol": symbol, "market": Config.MARKET_NAME})
+    #             datetime_latest = session.query(
+    #                 func.max(model_tmp.ts_start).label('ts_start_latest')
+    #             ).filter(
+    #                 model_tmp.symbol == symbol,
+    #                 model_tmp.market == Config.MARKET_NAME
+    #             ).scalar()
+    #             # issue:
+    #             # https://stackoverflow.com/questions/9882358/how-to-delete-rows-from-a-table-using-an-sqlalchemy-query-without-orm
+    #             delete_count = session.query(model_tmp).filter(
+    #                 model_tmp.market == Config.MARKET_NAME,
+    #                 model_tmp.symbol == symbol,
+    #                 model_tmp.ts_start < datetime_latest
+    #             ).delete()
+    #             self.logger.debug('%d 条 %s 历史数据被清理，最新数据日期 %s', delete_count, symbol, datetime_latest)
+    #             session.commit()
+    #         except:
+    #             self.logger.exception('%d 条 %s 数据-> %s 失败', md_count, symbol, model_tot.__tablename__)
 
 
 def start_supplier(init_symbols=False, do_fill_history=False) -> MDFeeder:
