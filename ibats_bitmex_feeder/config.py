ad9a85219f1c0054fd457fb8b5e2a53a79f3351a
mmmaaaggg <mmmaaaggg@163.com>
2018-10-29 14:36:03 +0800

    update db config

--- ibats_bitmex_feeder/config.py
+++ ibats_bitmex_feeder/config.py
@@ -8,7 +8,7 @@
 @desc    : 
 """
 import logging
-from ibats_common.config import ConfigBase as ConBase
+from ibats_common.config import ConfigBase as ConBase, update_db_config
 from ibats_common.common import ExchangeName
 
 logger = logging.getLogger()
@@ -67,3 +67,4 @@ def update_config(config_new: ConfigBase):
     global config
     config = config_new
     logger.info('更新默认配置信息 %s < %s', ConfigBase, config_new.__class__)
+    update_db_config(config.DB_URL_DIC)
